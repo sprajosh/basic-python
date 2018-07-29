949f8ed31b21a9b165902ce900b5e7e2fb5bea96
GokulDas027 <gokuldas027@gmail.com>
2018-07-29 22:36:28 +0530

    Fix: Fixed Typos and broken link

---
+++ JUP_OPEN.bat
@@ -0,0 +1,4 @@
+C:\Users\C4PTAIN\Anaconda3\python.exe C:\Users\C4PTAIN\Anaconda3\cwp.py C:\Users\C4PTAIN\Anaconda3 C:\Users\C4PTAIN\Anaconda3\python.exe C:\Users\C4PTAIN\Anaconda3\Scripts\jupyter-notebook-script.py "F:\0001 Goku\Basic Python\basic-python"
+
+
+
