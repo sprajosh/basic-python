68e71f613675f863b974406526325b3125903775
GokulDas027 <gokuldas027@gmail.com>
2018-10-07 22:36:38 +0530

    Removed Checkpoints

--- JUP_OPEN.bat
+++
@@ -1,4 +0,0 @@
-C:\Users\C4PTAIN\Anaconda3\python.exe C:\Users\C4PTAIN\Anaconda3\cwp.py C:\Users\C4PTAIN\Anaconda3 C:\Users\C4PTAIN\Anaconda3\python.exe C:\Users\C4PTAIN\Anaconda3\Scripts\jupyter-notebook-script.py "F:\0001 Goku\Basic Python\basic-python"
-
-
-
